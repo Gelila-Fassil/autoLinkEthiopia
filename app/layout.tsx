1bf4fba2c44d8b00c17e4693af94375b883e0b12
Seyfu Shibru <125612611+seyfushibru@users.noreply.github.com>
2026-09-10 10:14:57 +0300

    Add Google verification and AdSense meta tags
    Added Google site verification and AdSense meta tags.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   google-site-verification=Dqi3tQQ5vT4N0PEyEb_Zz4Oo_xMytjIvvEiSxxzIjaw
+<meta name="google-adsense-account" content="ca-pub-3745029649009326">
   title: "Autolink Ethiopia | Luxury Cars & High-End Rentals",
   description:
     "The premier destination for luxury automotive sales and exclusive property rentals in Ethiopia.",
